89f3cd7400004f52f273dfd6bf30ac4f998d7618
Nicolas <nicolas.migues97@gmail.com>
2025-11-08 13:38:56 -0300

    OkAgent

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class Agent : MonoBehaviour
+public abstract class Agent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Vector3 _velocity;
+    [SerializeField]protected float _maxVelocity;
+    [SerializeField]protected float _maxSteeringForce;
+    protected virtual void Update()
     {
-        
+        this.transform.position += _velocity * Time.deltaTime;
     }
-
-    // Update is called once per frame
-    void Update()
+    protected void AddForce(Vector3 dir)
     {
-        
+        _velocity = Vector3.ClampMagnitude(_velocity + dir, _maxVelocity);
+        _velocity.y = 0;
+    }
+    protected Vector3 Seek(Vector3 target)
+    {
+        var desired = target - this.transform.position;
+        desired.Normalize();
+        desired *= _maxVelocity;
+        var steer = desired - _velocity;
+        steer = Vector3.ClampMagnitude(steer, _maxSteeringForce);
+        return steer;
+    }
+    public void GetSeekForce(Vector3 target)
+    {
+        AddForce(Seek(target));
     }
 }
--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class Enemy : MonoBehaviour
+public class Enemy : Agent
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.Update();
     }
 }
