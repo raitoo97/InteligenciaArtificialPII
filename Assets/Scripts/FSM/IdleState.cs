021aa8c27689bbca2d7d00dd7309346323a13abe
Nicolas <nicolas.migues97@gmail.com>
2025-11-09 19:45:05 -0300

    OKINIT

---
+++ Assets/Scripts/FSM/IdleState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IState
+{
+    public void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Onstart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
