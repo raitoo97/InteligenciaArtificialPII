52313ac23151e574a940976d80879e0df4a49ca6
Nicolas <nicolas.migues97@gmail.com>
2025-11-09 02:07:32 -0300

    PrePathfinding

--- Assets/Scripts/Pahtfinding/Pahtfinding.cs
+++ Assets/Scripts/Pahtfinding/Pahtfinding.cs
@@ -1,7 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
 public static class Pahtfinding
 {
     public static List<Node> CalculateAStar(Node start,Node goal)
--- Assets/Scripts/Pahtfinding/PriorityQueue.cs
+++ Assets/Scripts/Pahtfinding/PriorityQueue.cs
@@ -1,17 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
-public static class PriorityQueue <T>
+public class PriorityQueue <T>
 {
-    private static Dictionary<T,float> _allElements = new Dictionary<T,float>();
-    public static int Count { get => _allElements.Count; }
-    public static void Enqueue(T key,float value)
+    private Dictionary<T,float> _allElements = new Dictionary<T,float>();
+    public int Count { get => _allElements.Count; }
+    public void Enqueue(T key,float value)
     {
         if(!_allElements.ContainsKey(key))
             _allElements.Add(key, value);
         else
             _allElements[key] = value;
     }
-    public static T Dequeue()
+    public T Dequeue()
     {
         T minElement = default;
         float minValue = Mathf.Infinity;
